415933fd27a4fe030ab5945a7d4954c7878e60f5
Emilie <49416341+elopez2019@users.noreply.github.com>
2024-10-19 03:24:51 +0000

    puerto config

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,11 @@ const mysql = require('mysql2'); // Asegúrate de estar usando mysql2 en lugar d
 const bcrypt = require('bcrypt');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
 
 // Configuración de la conexión a la base de datos
 const db = mysql.createConnection({
